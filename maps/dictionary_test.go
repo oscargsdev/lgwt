5228fa4f9c7954cb206e2e82448b1debb80f78bc
Oscar GS <oscargs.dev@gmail.com>
2025-07-27 13:59:18 -0600

    Maps

---
+++ maps/dictionary_test.go
@@ -0,0 +1,19 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertString(t, got, want)
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
