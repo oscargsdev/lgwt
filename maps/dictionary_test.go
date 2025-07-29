faeeea16c42f7a1984997600b5a82c3562f838ac
Oscar GS <oscargs.dev@gmail.com>
2025-07-28 20:00:09 -0600

    Add functionallity for Dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,28 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary.Add(word, definition)
+
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
 func assertString(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
@@ -34,4 +56,14 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word")
+	}
+
+	assertString(t, got, definition)
 }
