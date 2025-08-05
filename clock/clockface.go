f1d6ceca640fdd5bf0f5216dca0a3a44bcf9666a
Oscar GS <oscargs.dev@gmail.com>
2025-08-04 23:02:18 -0600

    Initial setup and acceptance test for clockface

---
+++ clock/clockface.go
@@ -0,0 +1,21 @@
+package clock
+
+import (
+	"math"
+	"time"
+)
+
+// A point represents a two-dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at the time t represented as a Point
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
+}
---
+++ clockface_test/clockface_test.go
@@ -0,0 +1,30 @@
+package clockface_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/oscargsdev/lgwt/clock"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clock.Point{X: 150, Y: 150 - 90}
+	got := clock.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := clock.Point{X: 150, Y: 150 + 90}
+	got := clock.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
