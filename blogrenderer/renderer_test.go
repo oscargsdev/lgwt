0ec698ef5bc3e19a3ab41d307965877842a4690e
Oscar GS <oscargs.dev@gmail.com>
2025-08-09 21:18:34 -0600

    Refactor to not parse templ every time

--- blogrenderer/renderer_test.go
+++ blogrenderer/renderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/approvals/go-approval-tests"
@@ -17,12 +18,33 @@ func TestRender(t *testing.T) {
 		Tags:        []string{"go", "tdd"},
 	}
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
 	t.Run("it converts s single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	aPost := blogrenderer.Post{
+		Title:       "hello world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for b.Loop() {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
