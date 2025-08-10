c2c319cc907e60557849c350e682110ddf77a806
Oscar GS <oscargs.dev@gmail.com>
2025-08-09 22:30:16 -0600

    Markdown parser

--- blogrenderer/renderer.go
+++ blogrenderer/renderer.go
@@ -5,6 +5,9 @@ import (
 	"html/template"
 	"io"
 	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 //go:embed "templates/*"
@@ -20,7 +23,8 @@ func (p Post) SanitisedTitle() string {
 }
 
 type PostRenderer struct {
-	templ *template.Template
+	templ    *template.Template
+	mdParser *parser.Parser
 }
 
 func NewPostRenderer() (*PostRenderer, error) {
@@ -29,11 +33,14 @@ func NewPostRenderer() (*PostRenderer, error) {
 		return nil, err
 	}
 
-	return &PostRenderer{templ: templ}, nil
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
-	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r)); err != nil {
 		return err
 	}
 
@@ -46,4 +53,15 @@ func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
 	}
 
 	return nil
+}
+
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/oscargsdev/lgwt
 
 go 1.24.5
 
-require github.com/approvals/go-approval-tests v1.6.0
+require (
+	github.com/approvals/go-approval-tests v1.6.0
+	github.com/gomarkdown/markdown v0.0.0-20250731182530-5d03d1963446
+)
