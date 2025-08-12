646a59536db70ed3759cd0a788bdf327b618a636
Oscar GS <oscargs.dev@gmail.com>
2025-08-11 19:48:58 -0600

    Generic Stack

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -16,7 +16,7 @@ func TestAssertFunctions(t *testing.T) {
 
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
+		myStackOfInts := new(Stack[int])
 		AssertTrue(t, myStackOfInts.IsEmpty())
 
 		myStackOfInts.Push(123)
@@ -32,7 +32,7 @@ func TestStack(t *testing.T) {
 	})
 
 	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(StackOfStrings)
+		myStackOfStrings := new(Stack[string])
 		AssertTrue(t, myStackOfStrings.IsEmpty())
 
 		myStackOfStrings.Push("123")
