b0d9e0f6ccc6bdd84cddd2316d4f1d6f8bcc43af
Oscar GS <oscargs.dev@gmail.com>
2025-08-11 19:16:27 -0600

    Naive generics

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -7,18 +7,23 @@ func TestAssertFunctions(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
 	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Vane")
+	})
 }
 
-func AssertEqual(t *testing.T, got, want int) {
+func AssertEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %d want %+v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want int) {
+func AssertNotEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got == want {
-		t.Errorf("didn't want %d", got)
+		t.Errorf("didn't want %+v", got)
 	}
 }
